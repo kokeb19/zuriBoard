1036b2986960961850c56970ff2f6748803b4d83
kokeb19 <88825511+kokeb19@users.noreply.github.com>
2022-06-25 09:49:07 -0400

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,4 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+console.log('Kokebe Negalign Mekuria')
+console.log('Height: 1.73cm');
+console.log('Country: Ethiopia');
